f96b03cd09b00e5fcf78c96b0cd70c2943ebf41d
Rodrigo Baldiviezo <rodrigo.baldiviezo@fundacion-jala.org>
2021-07-14 18:17:26 -0400

    Update using orders

--- resumes-api/src/api/Jalasoft.TeamUp.Resumes.API/Startup.cs
+++ resumes-api/src/api/Jalasoft.TeamUp.Resumes.API/Startup.cs
@@ -1,15 +1,16 @@
 ﻿using Jalasoft.TeamUp.Resumes.API;
-using Jalasoft.TeamUp.Resumes.Core;
-using Jalasoft.TeamUp.Resumes.Core.Interfaces;
-using Jalasoft.TeamUp.Resumes.DAL;
-using Jalasoft.TeamUp.Resumes.DAL.Interfaces;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 
 namespace Jalasoft.TeamUp.Resumes.API
 {
+    using Jalasoft.TeamUp.Resumes.Core;
+    using Jalasoft.TeamUp.Resumes.Core.Interfaces;
+    using Jalasoft.TeamUp.Resumes.DAL;
+    using Jalasoft.TeamUp.Resumes.DAL.Interfaces;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
