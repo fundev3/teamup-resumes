78c652606a1054834004a824fdf0e98a92492bef
Rodrigo Baldiviezo <85939959+rodrigo-baldiviezo-jalafund@users.noreply.github.com>
2021-07-15 14:00:28 -0400

    Feature-43 Add resumes list functionality (#3)
    * Add resumes list functionality
    
    * Add CORS configuration
    
    * Change return array and enumerable
    
    * Update resumes-api/src/dal/Jalasoft.TeamUp.Resumes.DAL/ResumesRepository.cs
    
    Co-authored-by: cristian-chavez-jalafund <85882117+cristian-chavez-jalafund@users.noreply.github.com>
    
    Co-authored-by: cristian-chavez-jalafund <85882117+cristian-chavez-jalafund@users.noreply.github.com>

---
+++ resumes-api/src/core/Jalasoft.TeamUp.Resumes.Core/ResumesService.cs
@@ -0,0 +1,22 @@
+﻿namespace Jalasoft.TeamUp.Resumes.Core
+{
+    using System.Linq;
+    using Jalasoft.TeamUp.Resumes.Core.Interfaces;
+    using Jalasoft.TeamUp.Resumes.DAL.Interfaces;
+    using Jalasoft.TeamUp.Resumes.Models;
+
+    public class ResumesService : IResumesService
+    {
+        private readonly IResumesRepository resumesRepository;
+
+        public ResumesService(IResumesRepository resumesRepository)
+        {
+            this.resumesRepository = resumesRepository;
+        }
+
+        public Resume[] GetResumes()
+        {
+            return this.resumesRepository.GetResumes().ToArray();
+        }
+    }
+}
---
+++ resumes-api/src/dal/Jalasoft.TeamUp.Resumes.DAL.Interfaces/IResumesRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Jalasoft.TeamUp.Resumes.DAL.Interfaces
+{
+    using System.Collections.Generic;
+    using Jalasoft.TeamUp.Resumes.Models;
+
+    public interface IResumesRepository
+    {
+        IEnumerable<Resume> GetResumes();
+    }
+}
