e54cbdec92941a579d440a6be72267e5824ca03b
Alex Flores <alex.flores@fundacion-jala.org>
2021-07-16 11:32:16 -0400

    feature13 test

---
+++ test-pipeline/test-pipeline/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test_pipeline
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo();
+            Console.WriteLine(rectangulo.CalcularArea(1, 2));
+        }
+
+        public class Rectangulo
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public int CalcularArea(int Widht, int Height)
+            {
+                return Widht * Height;
+            }
+        }
+
+
+
+    }
+}
