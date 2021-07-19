87f99dad0aaf11020d04487e741be0a45101074d
Rodrigo Baldiviezo <rodrigo.baldiviezo@fundacion-jala.org>
2021-07-19 16:49:17 -0400

    Add unittest in api and core layer

---
+++ resumes-api/tests/api/Jalasoft.TeamUp.Resumes.API.Tests/GetResumesTests.cs
@@ -0,0 +1,87 @@
+﻿namespace Jalasoft.TeamUp.Resumes.API.Tests
+{
+    using System;
+    using Jalasoft.TeamUp.Resumes.API.Controllers;
+    using Jalasoft.TeamUp.Resumes.Core.Interfaces;
+    using Jalasoft.TeamUp.Resumes.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using Xunit;
+
+    public class GetResumesTests
+    {
+        private readonly Mock<IResumesService> mockService;
+        private readonly Mock<HttpRequest> mockRequest;
+        private readonly GetResumes getResumes;
+
+        public GetResumesTests()
+        {
+            this.mockService = new Mock<IResumesService>();
+            this.mockRequest = new Mock<HttpRequest>();
+            this.getResumes = new GetResumes(this.mockService.Object);
+        }
+
+        [Fact]
+        public void GetResumes_Returns_OkObjectResult()
+        {
+            Assert.IsType<OkObjectResult>(this.getResumes.Run(this.mockRequest.Object));
+        }
+
+        [Fact]
+        public void GetResumes_Returns_NotOkResult()
+        {
+            Assert.IsNotType<CreatedResult>(this.getResumes.Run(this.mockRequest.Object));
+        }
+
+        [Fact]
+        public void GetTasks_Returns_EmptyResult()
+        {
+            this.mockService.Setup(service => service.GetResumes()).Returns(new Resume[0]);
+
+            var result = this.getResumes.Run(this.mockRequest.Object);
+
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            var resumes = Assert.IsType<Resume[]>(okObjectResult.Value);
+            Assert.Empty(resumes);
+        }
+
+        [Fact]
+        public void GetResumes_Returns_AllItems()
+        {
+            this.mockService.Setup(service => service.GetResumes()).Returns(this.GetTestResumes());
+
+            var result = this.getResumes.Run(this.mockRequest.Object);
+
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            var resumes = Assert.IsType<Resume[]>(okObjectResult.Value);
+            Assert.Equal(2, resumes.Length);
+        }
+
+        public Resume[] GetTestResumes()
+        {
+            var resumes = new Resume[]
+            {
+                new Resume()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Guido",
+                    LastName = "Castro",
+                    Email = "guido.castro@fundacion-jala.org",
+                    Phone = "123-456-789",
+                    Summary = "I'm a great developer :-D"
+                },
+                new Resume()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Marcelo",
+                    LastName = "Ruiz",
+                    Email = "marcelo.ruiz@fundacion-jala.org",
+                    Phone = "789-456-123",
+                    Summary = "I'm a great ux :-D"
+                }
+            };
+            return resumes;
+        }
+    }
+}
---
+++ resumes-api/tests/core/Jalasoft.TeamUp.Resumes.Core.Tests/ResumesServiceTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Jalasoft.TeamUp.Resumes.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Jalasoft.TeamUp.Resumes.DAL.Interfaces;
+    using Jalasoft.TeamUp.Resumes.Models;
+    using Moq;
+    using Xunit;
+
+    public class ResumesServiceTests
+    {
+        private readonly Mock<IResumesRepository> mockRepository;
+        private readonly ResumesService resumesService;
+
+        public ResumesServiceTests()
+        {
+            this.mockRepository = new Mock<IResumesRepository>();
+            this.resumesService = new ResumesService(this.mockRepository.Object);
+        }
+
+        [Fact]
+        public void GetTasks_Returns_EmptyResult()
+        {
+            this.mockRepository.Setup(repository => repository.GetResumes()).Returns(new List<Resume>());
+
+            var result = this.resumesService.GetResumes();
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetResumes_Returns_AllItems()
+        {
+            this.mockRepository.Setup(repository => repository.GetResumes()).Returns(this.GetTestResumes());
+
+            var result = this.resumesService.GetResumes();
+
+            Assert.Equal(2, result.Length);
+        }
+
+        public IEnumerable<Resume> GetTestResumes()
+        {
+            var resumes = new List<Resume>
+            {
+                new Resume()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Guido",
+                    LastName = "Castro",
+                    Email = "guido.castro@fundacion-jala.org",
+                    Phone = "123-456-789",
+                    Summary = "I'm a great developer :-D"
+                },
+                new Resume()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Marcelo",
+                    LastName = "Ruiz",
+                    Email = "marcelo.ruiz@fundacion-jala.org",
+                    Phone = "789-456-123",
+                    Summary = "I'm a great ux :-D"
+                }
+            };
+            return resumes;
+        }
+    }
+}
