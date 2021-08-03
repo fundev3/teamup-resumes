7d112bbc7fee74587d694d081017c053d09b86bb
Lina Ramirez Guizada <Lina.Ramirez@fundacion-jala.org>
2021-08-03 15:14:49 -0400

    Remove line

--- src/api/Jalasoft.TeamUp.Resumes.API/Startup.cs
+++ src/api/Jalasoft.TeamUp.Resumes.API/Startup.cs
@@ -19,8 +19,6 @@ namespace Jalasoft.TeamUp.Resumes.API
             builder.Services.AddScoped<IHealthService, HealthService>();
             builder.Services.AddScoped<IHealthRepository, HealthRepository>();
             builder.Services.AddScoped<IResumesService, ResumesService>();
-            builder.Services.AddScoped<IRepository<Resume>, ResumesInMemoryRepository>();
-
 #if DEBUG
             builder.Services.AddScoped<IRepository<Resume>, ResumeSQLRepository>();
 #else
