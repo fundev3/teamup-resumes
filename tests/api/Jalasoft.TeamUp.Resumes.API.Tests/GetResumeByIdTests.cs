7704e9d68f5932ce6266e21a0804bdf6898404b6
Paola.Quintanilla <74014.linares@gmail.com>
2021-08-05 13:29:49 -0400

    fix test

--- tests/api/Jalasoft.TeamUp.Resumes.API.Tests/GetResumeByIdTests.cs
+++ tests/api/Jalasoft.TeamUp.Resumes.API.Tests/GetResumeByIdTests.cs
@@ -46,7 +46,7 @@
         {
             // Arrange
             var request = this.mockHttpContext.Request;
-            this.mockService.Setup(service => service.GetResume(Guid.Parse("5a7939fd-59de-44bd-a092-f5d8434584de"))).Throws(new ResumeException(ErrorsTypes.NotFoundError));
+            this.mockService.Setup(service => service.GetResume(Guid.Parse("5a7939fd-59de-44bd-a092-f5d8434584de"))).Throws(new ResumeException(ErrorsTypes.NotFoundError, new Exception()));
 
             // Act
             var response = this.getResume.Run(request, new Guid("5a7939fd-59de-44bd-a092-f5d8434584de"));
