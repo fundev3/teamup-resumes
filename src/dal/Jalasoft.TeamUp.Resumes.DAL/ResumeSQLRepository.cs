28f9271064eff5d7dbbf5d7e97f863b6802ba5cb
Lina Ramirez Guizada <Lina.Ramirez@fundacion-jala.org>
2021-08-13 17:52:25 -0400

    Modify query to get Skill

--- src/dal/Jalasoft.TeamUp.Resumes.DAL/ResumeSQLRepository.cs
+++ src/dal/Jalasoft.TeamUp.Resumes.DAL/ResumeSQLRepository.cs
@@ -49,7 +49,13 @@
             IEnumerable<Resume> resumes;
             using (IDbConnection db = new SqlConnection(this.connectionString))
             {
-                string sql = "SELECT * FROM Resume r JOIN Person p on r.IdPerson = p.Id JOIN Contact c on r.IdContact = c.Id JOIN Resume_Skill rs on r.Id = rs.IdResume JOIN Skill s on rs.IdSkill = s.Id ";
+                string sql = "SELECT r.Id, r.Title, r.Sumary, r.CreationDate, r.LastUpdate, r.IdPerson, r.IdContact, p.Id, p.FirstName, " +
+                    "p.LastName, p.BirthDate, p.Picture, c.Id, c.Address, c.Email, c.Phone, rs.IdSkill, rs.IdResume, s.Id, s.Name " +
+                    "FROM Resume r " +
+                    "JOIN Person p ON r.IdPerson = p.Id " +
+                    "JOIN Contact c ON r.IdContact = c.Id " +
+                    "JOIN Resume_Skill rs ON r.Id = rs.IdResume " +
+                    "JOIN Skill s ON rs.IdSkill = s.Id ";
 
                 var resumesAux = db.Query<Resume, Person, Contact, Skill, Resume>(sql, (resume, person, contact, skill) =>
                 {
