047cfddded353bbdbf9568c4a54d816dce40182f
Gustavo Rojas Valdivia <Gustavo.Rojas@fundacion-jala.org>
2021-08-24 19:17:29 -0400

    Get Resume by Name

--- tests/api/Jalasoft.TeamUp.Resumes.API.Tests/GetResumesTests.cs
+++ tests/api/Jalasoft.TeamUp.Resumes.API.Tests/GetResumesTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Jalasoft.TeamUp.Resumes.API.Tests
 {
     using System;
+    using System.Web;
     using Jalasoft.TeamUp.Resumes.API.Controllers;
     using Jalasoft.TeamUp.Resumes.Core.Interfaces;
     using Jalasoft.TeamUp.Resumes.Models;
@@ -38,33 +39,18 @@
         }
 
         [Fact]
-        public void GetResumes_InvalidResponse_Error500()
+        public void GetResumes_WithQueryParam_Resume()
         {
             // Arrange
             var request = this.mockHttpContext.Request;
-            var res = this.mockService.Setup(service => service.GetByName(null)).Throws(new ResumesException(ResumesErrors.InternalServerError, new Exception()));
+            this.mockService.Setup(service => service.GetByName("Gustavo")).Returns(new Resume[10]);
 
             // Act
             var response = this.getResumes.Run(request);
 
             // Assert
-            var objectResult = Assert.IsType<ObjectResult>(response);
-            Assert.Equal(500, objectResult.StatusCode);
-        }
-
-        [Fact]
-        public void GetResumes_InvalidResume_NotFound()
-        {
-            // Arrange
-            var request = this.mockHttpContext.Request;
-            this.mockService.Setup(service => service.GetByName("none")).Throws(new ResumesException(ResumesErrors.NotFound));
-
-            // Act
-            var response = this.getResumes.Run(request);
-
-            // Assert
-            var notFound = Assert.IsType<ObjectResult>(response);
-            Assert.Equal(404, notFound.StatusCode);
+            var okObjectResult = Assert.IsType<OkObjectResult>(response);
+            Assert.IsType<Resume[]>(okObjectResult.Value);
         }
     }
 }
