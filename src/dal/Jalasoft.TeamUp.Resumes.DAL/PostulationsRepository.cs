b39179fdb48dcd025fcfd07c7bdf8a11ce3ce5be
Paola.Quintanilla <74014.linares@gmail.com>
2021-08-25 07:56:44 -0400

    delete comments

--- src/dal/Jalasoft.TeamUp.Resumes.DAL/PostulationsRepository.cs
+++ src/dal/Jalasoft.TeamUp.Resumes.DAL/PostulationsRepository.cs
@@ -32,21 +32,9 @@
                 state = postulation.State
             };
 
-            // Postulation result = new Postulation();
-            // var sql = "INSERT INTO Resume ( ProjectId, ResumeId, ProjectName, ResumeName, CreationDate, LastUpdate ) VALUES (@projectId, @resumeId, @projectName, @resumeName, @creationdate, @lastupdate)";
             using (IDbConnection db = new SqlConnection(this.connectionString))
             {
                 db.Execute(storeProcedure, values, commandType: CommandType.StoredProcedure);
-
-                // db.Open();
-                // DynamicParameters parameter = new DynamicParameters();
-                // parameter.Add("@projectId", postulation.ProjectId, DbType.Int32, ParameterDirection.Input, 30);
-                // parameter.Add("@resumeId", postulation.ResumeId, DbType.Int32, ParameterDirection.Input, 30);
-                // parameter.Add("@projectName", postulation.ProjectName, DbType.AnsiString, ParameterDirection.Input, 150);
-                // parameter.Add("@resumeName", postulation.ResumeName, DbType.AnsiString, ParameterDirection.Input, 150);
-                // parameter.Add("@resumeName", DateTime.Now, DbType.DateTime, ParameterDirection.Input);
-                // parameter.Add("@lastupdate", DateTime.Now, DbType.DateTime, ParameterDirection.Input);
-                // db.Execute(sql, parameter);
             }
 
             return postulation;
