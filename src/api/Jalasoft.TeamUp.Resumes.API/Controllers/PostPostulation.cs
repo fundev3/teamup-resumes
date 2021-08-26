11145b804ec809bfc7b12703c24b410505e36096
Alex Flores <alex.flores@fundacion-jala.com>
2021-08-26 10:03:43 -0400

    some corrections

--- src/api/Jalasoft.TeamUp.Resumes.API/Controllers/PostPostulation.cs
+++ src/api/Jalasoft.TeamUp.Resumes.API/Controllers/PostPostulation.cs
@@ -29,7 +29,7 @@ namespace Jalasoft.TeamUp.Resumes.API.Controllers
         [OpenApiRequestBody("application/json", typeof(Postulation), Description = "JSON request body containing Postulation")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.Created, contentType: "application/json", bodyType: typeof(Postulation), Description = "Successful response")]
         public IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/postulations")] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/postulations")] HttpRequest req)
         {
             try
             {
