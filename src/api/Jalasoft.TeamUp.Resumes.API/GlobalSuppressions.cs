cc1532dd8e721a4db137b4cddbf464d42ac2aea9
Cristian Chavez Salazar <cristian.chavez@fundacion-jala.org>
2021-08-30 17:21:14 -0400

    downgrade to 0.5.1-preview, ignoring stylecop

---
+++ src/api/Jalasoft.TeamUp.Resumes.API/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "OpenApi package downgrade")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "OpenApi package downgrade", Scope = "type", Target = "~T:Microsoft.Azure.WebJobs.Extensions.OpenApi.OpenApiHttpTriggerContext")]
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1210:Using directives should be ordered alphabetically by namespace", Justification = "OpenApi package downgrade")]
+[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "OpenApi package downgrade", Scope = "type", Target = "~T:Microsoft.Azure.WebJobs.Extensions.OpenApi.OpenApiHttpTriggerContext")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "OpenApi package downgrade", Scope = "type", Target = "~T:Microsoft.Azure.WebJobs.Extensions.OpenApi.OpenApiHttpTrigger")]
+[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "OpenApi package downgrade", Scope = "type", Target = "~T:Microsoft.Azure.WebJobs.Extensions.OpenApi.OpenApiHttpTrigger")]
